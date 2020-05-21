7abe304e847824236574240db357a175e25bbcc8
Ambar Rodriguez <ambarrodriguez80@yahoo.com>
2020-05-21 15:04:17 -0600

    Use single print statements.

--- src/edu/cnm/deepdive/FizzBuzz.java
+++ src/edu/cnm/deepdive/FizzBuzz.java
@@ -1,28 +1,23 @@
 package edu.cnm.deepdive;
 
 public class FizzBuzz {
+
   public static void main(String[] args) {
 
     for (int counter = 1; counter < 100; counter++) {
-      // - CHeck whether the current counter value is fizz, buzz or both.
-      // - Print the number, or fizz, or buzz or fizzbuzz, as appropriate.
+      String status = "";
       boolean counterIsFizz = (counter % 3 == 0);
       boolean counterIsBuzz = (counter % 5 == 0);
-
-//TODO Compute and assign boolean values to counterIsFizz and counterIsBuzz
-      // counter is divisible by 3 and 5.
-      // otherwise, if counterIsfizz then print fizz;
-      // otherwise, if counterIsBuzz, then print buzz;
-      // otherwise, print counter.
-      if (counterIsFizz && counterIsBuzz) {
-        System.out.println("fizzbuzz");
-      } else if (counterIsFizz){
-        System.out.println("fizz");
-      } else if (counterIsBuzz) {
-        System.out.println("buzz");
-      } else {
-        System.out.println(counter);
+      if (counterIsFizz) {
+        status += "fizz";
       }
+      if (counterIsBuzz) {
+        status += "buzz";
+      }
+      if (status.isEmpty()) {
+        status += counter;
+      }
+      System.out.println(status);
     }
   }
 
